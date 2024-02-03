a99c533ac4cf5e001f7ba259063c9658d45d7de6
Ésley Nathan <esleynathan@hotmail.com>
2024-02-03 18:27:53 -0300

    Diretivas condicionais | Diretivas v-for

--- src/components/LogarButton.vue
+++ src/components/LogarButton.vue
@@ -1,9 +1,47 @@
 <template>
-    <button>Logar</button>    
+    <div v-if="local === 'materiais'"> Você esta nos materiais</div>
+    <div v-else-if="local === 'sobre'"> Você esta no sobres</div>
+    <div v-else> Você esta no duvidas</div>
+    <hr>
+    <br>
+    <br>
+
+    <div 
+    v-for="(curso, index) in cursos"
+    v-bind:key="curso.nome"
+    >   
+        <p>{{ index }}</p>
+        <p>{{ curso.nome }}</p>
+        <p>{{ curso.qtdAulas }}</p>
+        <p>{{ curso.duracao }}</p>
+        <hr>
+    </div>
+
+
 </template>
 
 <script>
     export default{
+        data(){
+            return {
+                local: 'sobre',
+                cursos: [
+                    {
+                        nome: 'Python full',
+                        qtdAulas: '600',
+                        duracao: '110',                    
+                    },
+                    {   nome: 'Vue full',
+                        qtdAulas: '100',
+                        duracao: '18',                    
+                    },
+                    {   nome: 'IA full',
+                        qtdAulas: '400',
+                        duracao: '20',                    
+                    },
+                ],
+            }
+        }
 
     }
 

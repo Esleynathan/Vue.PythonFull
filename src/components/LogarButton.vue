75f92f5bad79106da4332d12d16a47e37b55de82
Ésley Nathan <esleynathan@hotmail.com>
2024-02-14 20:44:04 -0300

    Watch

--- src/components/LogarButton.vue
+++ src/components/LogarButton.vue
@@ -1,32 +1,51 @@
 <template>
-    <span> {{isAluno}}</span>
+    <div>
+        <p>Mensagens</p>
+        <div id="mensagens">
+
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
     data() {
         return {
-            usuario: {
-                nome: 'Caio Sampaio',
-                cursos: [
-                'Python Full',
-                'Vue Full'
-            ]
+            novaMensagem: ""
         }
-    }
     },
 
     methods: {
+        gerarStringAleatoria() {
+            const letras = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+            let resultado = '';
+            for (let i = 0; i < 10; i++) {
+            resultado += letras.charAt(Math.floor(Math.random() * letras.length));
+            }
+            return resultado;
+        },
+
+        addMessage(){
+            this.mensagens = document.getElementById('mensagens')
+            this.mensagens.innerHTML += '<span>' + this.novaMensagem + '</span><hr>'
+        }
 
     },
 
-    computed: {
-        isAluno(){
-            return this.usuario.cursos.length > 0 ? 'Já é aluno' : 'Não é aluno'
+    watch: {
+        novaMensagem(){
+            this.addMessage();
         }
+    },
+
+    mounted(){
+        setInterval(() => {
+            this.novaMensagem = this.gerarStringAleatoria();
+        }, 5000);        
     }
 };
 </script>
+
 
 <style scoped>
     button{
